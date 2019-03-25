1444e54e2d4c2322977a218b8d642875f6ca7a36
prinned <vivekrip0@gmail.com>
2019-03-25 22:29:55 +0530

    git commit

---
+++ hangman.py
@@ -0,0 +1,99 @@
+'''
+Program to play single player hangman with computer generated words.
+Online text file the words came from: https://raw.githubusercontent.com/paritytech/wordlist/master/res/wordlist.txt
+Art made by yours truly! 
+'''
+
+import functools, string, re, random, requests
+
+class hang:
+    website='https://raw.githubusercontent.com/paritytech/wordlist/master/res/wordlist.txt'
+
+    
+    man=['\n\n\n\n\n\n\n\n\n',
+         '\n ___________\n|\n|\n|\n|\n|\n|\n\n',
+         '\n ___________\n|           |\n|           |\n|           |\n|           |\n|           |\n|___________|\n',
+         '\n ___________\n|     |     |\n|           |\n|           |\n|           |\n|           |\n|___________|\n',
+         '\n ___________\n|     |     |\n|     0     |\n|           |\n|           |\n|           |\n|___________|\n',
+         '\n ___________\n|     |     |\n|     0     |\n|     |     |\n|           |\n|           |\n|___________|\n',
+         '\n ___________\n|     |     |\n|     0     |\n|     |     |\n|    / \\    |\n|           |\n|___________|\n',
+         '\n ___________\n|     |     |\n|    \\0/    |\n|     |     |\n|    / \\    |\n|           |\n|___________|\n',
+         '\n ___________\n| | | | | | |\n| | |\\0/| | |\n| | | | | | |\n| | |/|\\| | |\n| | | | | | |\n|_|_|_|_|_|_|\n',
+         '\n ___________\n| | | | | | |\n| | |\\0/| | |       Gregory is forever forgotten.\n| | | | | | |       His wife wrongly believes he was a  \n| | |/|\\| | |       traitor from the start yet never \n| | | | | | |       moves on.\n|_|_|_|_|_|_|\n',
+         '\n     _\n  0 /0\\  Gregor thanks you for \n /|\\/|\\  uniting him with his wife!\n  |  Δ   They live happily\n / \\/ \\  ever after!\n']
+        
+    hide=lambda word, hints: re.sub(("[^"+functools.reduce(lambda a,b:a+b, hints)+"]"), '_', word)
+    
+    def __init__(self=None):
+        print('Accessing online directory... (May take up to a minute)')
+        global word_list
+        word_list=requests.get(hang.website).text.splitlines()
+        print('Success!')
+        
+    def mword(self=None):
+        length=1
+        while length<4:
+            hang.word=random.choice(word_list).lower()
+            length=len(hang.word)
+
+        global hints
+        hints = [random.choice(string.ascii_lowercase) for i in range(len(hang.word)//4+1)]
+
+        i=0
+        while i <len(hints):     #checks if the hint is in the word
+            if not hints[i] in hang.word:
+                hints[i]=random.choice(string.ascii_lowercase)
+                i-=1
+            i+=1
+        
+        hang.hid = hang.hide(hang.word,hints)
+
+    def guess(self=None,k=0):
+        if k>8 or k<1:
+            print('Invalid! k is out of bounds!')
+            return
+        ihint=hints
+        guessed=[]
+        i=8-k
+        while i<8:
+            print(hang.man[i])
+            print(hang.hid.replace("", " ")[1: -1])
+            x=input("Enter your guess: ")
+            if x=='':
+                continue
+            elif x in ihint:
+                print('That was the hint! Pay attention!')
+            elif x in guessed:
+                print("You've already guessed that, dummy!")
+                continue
+            elif len(x)>1:
+                print("Word guess!")
+            elif x in hang.word:
+                hints.append(x)
+                hang.hid = hang.hide(hang.word,hints)
+                print('Correct!')
+                guessed.append(x)
+                if hang.hid==hang.word:
+                    print("You've won!")
+                    print(hang.man[10])
+                    del i
+                    break
+            else: 
+                print('Wrong!')
+                i+=1
+                guessed.append(x)
+                if i==8:
+                    print(hang.man[9])
+                    print('Greogory is dead thanks to you.')
+                    del i
+                    break
+
+
+if __name__=="__main__":
+    hang()
+    x='y'
+    while x=='y':        
+        hang.mword()
+        k=input('How many turns would you like?')
+        hang.guess()
+        x=input('Reset? (y/n) ').lower()
---
+++ tictac.py
@@ -0,0 +1,110 @@
+class player:
+    r=[[' ',' ',' '],
+       [' ',' ',' '],
+       [' ',' ',' ']]
+    pni=1
+    global rp
+    
+    def __init__(self, symbol):
+        self.symbol=symbol
+
+        if symbol == ' ':
+            print('Error! Invalid symbol! Please choose another one.')
+            return
+        
+        if player.pni >2:
+            print('Error! Too many players!')
+            self.symbol=' '
+            return
+
+        self.pin=player.pni
+        print('You are Player ', self.pin,'!',sep='')
+
+        if player.pni==2:
+            print('Good Luck, both of you! Your turn, Player 1!')
+            player.display()
+
+        player.pni+=1
+
+    def display():
+        print(0,'|', player.r[0][0], player.r[0][1], player.r[0][2])
+        print(1,'|', player.r[1][0], player.r[1][1], player.r[1][2])
+        print(2,'|', player.r[2][0], player.r[2][1], player.r[2][2], '', sep='_')
+        print('  | 0 1 2')
+        
+        #checking for win
+        #sleeping win
+        if ((player.r[0][0] == player.r[0][1] and player.r[0][1] == player.r[0][2] and player.r[0][0]!=' ') or 
+            (player.r[1][0] == player.r[1][1] and player.r[1][1] == player.r[1][2] and player.r[1][0]!=' ') or 
+            (player.r[2][0] == player.r[2][1] and player.r[2][1] == player.r[2][2] and player.r[2][0]!=' ')):
+            print('\n',rp.symbol,' has WON! Press Enter to Close.',sep="")
+            d=input()
+            close=True
+            
+        
+        #standing win
+        elif ((player.r[0][0] == player.r[1][0] and player.r[0][0] == player.r[2][0] and player.r[0][0]!=' ') or 
+              (player.r[0][1] == player.r[1][1] and player.r[0][1] == player.r[2][1] and player.r[0][1]!=' ') or 
+              (player.r[0][2] == player.r[1][2] and player.r[0][2] == player.r[2][2] and player.r[0][2]!=' ')):
+            print('\n',rp.symbol,' has WON! Press Enter to Close.',sep="")
+            d=input()
+            close=True
+            
+        #diagnol win
+        elif ((player.r[0][0] == player.r[1][1] and player.r[0][0] == player.r[2][2] and player.r[0][0]!=' ') or 
+              (player.r[0][2] == player.r[1][1] and player.r[0][2] == player.r[2][0] and player.r[0][2]!=' ')):
+            print('\n',rp.symbol,' has WON! Press Enter to Close.',sep="")
+            d=input()
+            close=True
+
+        #draw
+        for i in player.r:
+            if ' ' in i:
+                return
+        d=input('DRAW! Press Enter to Close.')
+        close=True
+        
+
+    def play(self, x, y):
+        if player.pni <3:
+            print('You can\'t play alone, silly! Get a friend!')
+            return
+        
+        if player.r[x][y] != ' ':
+            print("That move's already been played, ", self.symbol,"!",sep="")
+            return
+        
+        global rp
+        try:
+            if rp==self:
+                print('Its not your turn, ',self.symbol,'!',sep='')
+                return
+        except:
+            if self.pin!=1:
+                print('Its not your turn, ',self.symbol,'!',sep='')
+                return
+        rp=self
+        
+        player.r[x][y]= self.symbol
+        player.display()
+    
+    def restart(self):
+        r=[[' ',' ',' '],
+           [' ',' ',' '],
+           [' ',' ',' ']]
+        pni=1
+        global rp
+        del self.symbol
+
+
+if __name__=='__main__':
+    player.display()
+    type_of_display = input('Are you running this program directly or using an IDLE? (d for direct, i for IDLE) ')
+    if type_of_display=='d':
+        close=False
+        while close==False:
+            try:
+                x= input('$ ')
+                exec(x)
+            except:
+                print('Unexpected Error!')
